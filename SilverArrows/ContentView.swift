90b6a04f663d77aed20a908af04df67ba86eefed
Hendrik Jahns <hendrik.jahns@outlook.com>
2022-08-15 20:05:41 +0200

    Initial Commit

---
+++ SilverArrows/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SilverArrows
+//
+//  Created by Hendrik Jahns on 15.08.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SilverArrows/SilverArrowsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SilverArrowsApp.swift
+//  SilverArrows
+//
+//  Created by Hendrik Jahns on 15.08.22.
+//
+
+import SwiftUI
+
+@main
+struct SilverArrowsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
