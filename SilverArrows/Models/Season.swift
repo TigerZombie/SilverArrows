f9b0a2e23e0d3c14848a29f6ce361277d7dba9f8
Hendrik Jahns <hendrik.jahns@outlook.com>
2022-08-17 21:15:39 +0200

    Create and add JSON file Create Season Model

---
+++ SilverArrows/Models/Season.swift
@@ -0,0 +1,22 @@
+//
+//  Season.swift
+//  SilverArrows
+//
+//  Created by Hendrik Jahns on 17.08.22.
+//
+
+import Foundation
+
+class SeasonJSON: Identifiable, Decodable {
+    
+    var id: Int
+    var image: String
+    var name: String
+    var season: String
+    var drivers: [String]
+    var championshipPoints: String
+    var teamChampionship: Bool
+    var driversChampionship: [Bool:String]
+      
+    
+}
