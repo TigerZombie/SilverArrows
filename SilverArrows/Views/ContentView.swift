61016622a8cb2deb0e1684873c49ce7aa088d205
Hendrik Jahns <hendrik.jahns@outlook.com>
2022-08-21 16:30:17 +0200

    Update to Custom font size Include cardView in ContentView -> ForEach is working The shown picture is not in the picture property of the JSON, but is a combination of JSON season and JSON name :)
    Stage GOLD completed

--- SilverArrows/Views/ContentView.swift
+++ SilverArrows/Views/ContentView.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @ObservedObject var season = SeasonModel()
+    @ObservedObject var F1season = SeasonModel()
+
     
     var body: some View {
         
@@ -25,12 +26,52 @@ struct ContentView: View {
                     .padding(.bottom, 16)
                 
                     
-                    ForEach (season.seasons) { s in
-                        Text(s.name)
+                    ForEach (F1season.seasons) { s in
+                     
+                        ZStack {
+                           
+                        ZStack (alignment: .leading){
                         
+                        HStack (spacing: 0){
+                        RoundedCornersLeft()
+                                .foregroundColor(.white)
+                                .frame(width: 132)
+                                .shadow(color: CustomColors.cardShadow, radius: 4, x: 4, y: 4)
+                            ZStack{
+                            RoundedCornersRight()
+                                    .fill(CustomColors.cardBackground)
+                                    .shadow(color: CustomColors.cardShadow, radius: 4, x: 4, y: 4)
+                            VStack {
+                                Text(s.name)
+                                    .font(.customFontTitle)
+                                    .foregroundColor(CustomColors.cardText)
+                                    
+                                Text("\(s.season) season")
+                                    .font(.customFontBody)
+                                    .foregroundColor(CustomColors.cardText)
+                            }
+                            }
+                                
+                                .frame(width: 198)
+                        }
+                        .frame(height: 95)
+                        
+                            
+                            Image("\(s.season) \(s.name)")
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: 132, height: 95)
+                                .clipShape(RoundedCornersLeft())
+                                .shadow(color: .black, radius: 4, x: 4)
+                                    .mask(Rectangle().padding(.trailing, -10))
+                    }
+                        
+                        .padding(.horizontal)
+                        .padding(.bottom, 8)
+                    }
+                      
                     }
                     
-                    //CardView(carName: s.name, season: s.season)
                     
                 }
                 }
