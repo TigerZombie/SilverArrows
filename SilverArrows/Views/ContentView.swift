3a0e850fb330aa06329ea5321e98db38e72e5b80
Hendrik Jahns <hendrik.jahns@outlook.com>
2022-08-31 20:10:01 +0200

    Implement collapsed/extended card feature

--- SilverArrows/Views/ContentView.swift
+++ SilverArrows/Views/ContentView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
     
     @ObservedObject var F1season = SeasonModel()
     
-    @State private var isSelected: Bool = false
+    @State private var idSelection: Int = 0
     
     @State private var pictureWidthUnselected: CGFloat = 132
     @State private var pictureHeightUnselected: CGFloat = 95
@@ -34,6 +34,8 @@ struct ContentView: View {
                 
                 // Stack of logo and cards
                 VStack{
+                    
+                    
                     Image("LogoAMGPetronas")
                         .resizable()
                         .scaledToFit()
@@ -43,7 +45,109 @@ struct ContentView: View {
                     // List of cards
                     ForEach (F1season.seasons) { s in
                         
+                        if idSelection == s.id {
+                        // View for extended card
+                        ZStack {
+                            
+                            ZStack (alignment: .top){
+                                
+                                
+                                VStack (spacing: 0){
+                                    
+                                    // invisible background item on left side, to align the right side item (the part with text). Also: Will provide the shadow below the picture.
+                                    RoundedCornersTop()
+                                        .foregroundColor(.white)
+                                        .frame(width: 330, height: 129)
+                                        .shadow(color: CustomColors.cardShadow, radius: 4, x: 4, y: 4)
+                                    
+                                    
+                                    ZStack{
+                                        
+                                        // the part of the card with text
+                                        RoundedCornersBottom()
+                                            .fill(CustomColors.cardBackground)
+                                            .shadow(color: CustomColors.cardShadow, radius: 4, x: 4, y: 4)
+                                        VStack  {
+                                            Text(s.name)
+                                                .font(.customFontTitle)
+                                                .foregroundColor(CustomColors.cardText)
+                                            
+                                            Text("\(s.season) season")
+                                                .font(.customFontBody)
+                                                .foregroundColor(CustomColors.cardText)
+                                            
+                                            VStack (alignment: .leading) {
+                                                HStack {
+                                                    Text("Drivers:")
+                                                    ForEach (s.drivers, id: \.self) { driver in
+                                                        Text("\(driver)")
+                                                    }
+                                                }
+                                                .font(.customFontBody)
+                                                .foregroundColor(CustomColors.cardText)
+                                                    
+                                                    HStack {
+                                                        Text("Championship Points:")
+                                                        Text(s.championshipPoints)
+                                                    }
+                                                    .font(.customFontBody)
+                                                    .foregroundColor(CustomColors.cardText)
+                                                    
+                                                if s.teamChampionship == true {
+                                                    HStack {
+                                                        Text("Team Championship")
+                                                        Image(systemName: "rosette")
+                                                            .foregroundColor(Color.yellow)
+                                                    }
+                                                    .font(.customFontBody)
+                                                    .foregroundColor(CustomColors.cardText)
+                                                }
+                                                    
+                                                if s.driversChampionship == true {
+                                                    HStack {
+                                                        Text("Driver Champion: \(s.driverChampion)")
+                                                        Image(systemName: "rosette")
+                                                            .foregroundColor(Color.yellow)
+                                                    }
+                                                    .font(.customFontBody)
+                                                    .foregroundColor(CustomColors.cardText)
+                                                }
+                                                
+                                            
+                                            }
+                                            .padding(.top, 10)
+                                        }
+                                        .frame(maxWidth: .infinity, alignment: .leading)
+                                        .padding(.horizontal)
+                                        
+                                    }
+                                    
+                                    .frame(width: 330)
+                                }
+                                
+                                // Height of complete card
+                                .frame(height: 280)
+                                
+                                // Image with shadow only to the right side (cropped with mask modifier)
+                                Image("\(s.season) \(s.name)")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .frame(width: pictureWidthSelected, height: pictureHeightSelected)
+                                    .clipShape(RoundedCornersTop())
+                                    .shadow(color: .black, radius: 4, x: 0, y: 4)
+                                    .mask(Rectangle().padding(.bottom, -14))
+                                    
+                            }
+                            
+                            .padding(.horizontal)
+                            .padding(.bottom, 8)
+                        }
+                        
+                        }
+                        
+                        else {
                         // View for collapsed card
+                       
                         ZStack {
                             
                             ZStack (alignment: .leading){
@@ -94,99 +198,13 @@ struct ContentView: View {
                             .padding(.horizontal)
                             .padding(.bottom, 8)
                         }
-                        
-                        // View for extended card
-                        ZStack {
-                            
-                            ZStack (alignment: .top){
-                                
-                                
-                                VStack (spacing: 0){
-                                    
-                                    // invisible background item on left side, to align the right side item (the part with text). Also: Will provide the shadow below the picture.
-                                    RoundedCornersTop()
-                                        .foregroundColor(.white)
-                                        .frame(width: 330, height: 129)
-                                        .shadow(color: CustomColors.cardShadow, radius: 4, x: 4, y: 4)
-                                    
-                                    
-                                    ZStack{
-                                        
-                                        // the part of the card with text
-                                        RoundedCornersBottom()
-                                            .fill(CustomColors.cardBackground)
-                                            .shadow(color: CustomColors.cardShadow, radius: 4, x: 4, y: 4)
-                                        VStack  {
-                                            Text(s.name)
-                                                .font(.customFontTitle)
-                                                .foregroundColor(CustomColors.cardText)
-                                            
-                                            Text("\(s.season) season")
-                                                .font(.customFontBody)
-                                                .foregroundColor(CustomColors.cardText)
-                                            
-                                            VStack (alignment: .leading) {
-                                                HStack {
-                                                    Text("Drivers:")
-                                                    ForEach (s.drivers, id: \.self) { driver in
-                                                        Text("\(driver)")
-                                                    }
-                                                }
-                                                .font(.customFontBody)
-                                                .foregroundColor(CustomColors.cardText)
-                                                    
-                                                    HStack {
-                                                        Text("Championship Points:")
-                                                        Text(s.championshipPoints)
-                                                    }
-                                                    .font(.customFontBody)
-                                                    .foregroundColor(CustomColors.cardText)
-                                                    
-                                                    HStack {
-                                                        Text("Team Championship")
-                                                        Image(systemName: "star")
-                                                            .foregroundColor(Color.yellow)
-                                                    }
-                                                    .font(.customFontBody)
-                                                    .foregroundColor(CustomColors.cardText)
-                                                    
-                                                    HStack {
-                                                        Text("Driver Champion: \(s.driverChampion)")
-                                                        Image(systemName: "star")
-                                                            .foregroundColor(Color.yellow)
-                                                    }
-                                                    .font(.customFontBody)
-                                                    .foregroundColor(CustomColors.cardText)
-                                              
-                                            
-                                            }
-                                            .padding(.top, 10)
-                                        }
-                                        .frame(maxWidth: .infinity, alignment: .leading)
-                                        .padding(.horizontal)
-                                        
-                                    }
-                                    
-                                    .frame(width: 330)
-                                }
-                                
-                                // Height of complete card
-                                .frame(height: 280)
-                                
-                                // Image with shadow only to the right side (cropped with mask modifier)
-                                Image("\(s.season) \(s.name)")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fill)
-                                    .frame(width: pictureWidthSelected, height: pictureHeightSelected)
-                                    .clipShape(RoundedCornersTop())
-                                    .shadow(color: .black, radius: 4, x: 0, y: 4)
-                                    .mask(Rectangle().padding(.bottom, -14))
-                                    
-                            }
-                            
-                            .padding(.horizontal)
-                            .padding(.bottom, 8)
+                        .onTapGesture {
+                            idSelection = s.id
                         }
+                        
+                        }
+                        
+                      
                         
                     }
                     
