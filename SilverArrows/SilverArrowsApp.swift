cf5edb8a818a139c8c3ed2a06aee3ecf3bd3ded7
Hendrik Jahns <hendrik.jahns@outlook.com>
2022-09-14 20:40:59 +0200

    Include plist

--- SilverArrows/SilverArrowsApp.swift
+++ SilverArrows/SilverArrowsApp.swift
@@ -12,8 +12,8 @@ struct SilverArrowsApp: App {
     var body: some Scene {
         WindowGroup {
 
-            
-            ContentView(SeasonToPass: Season(id: 1, image: "", name: "MGP W01", season: "2010", drivers: ["Nico", "Michael"], championshipPoints: "214", teamChampionship: false, driversChampionship: false, driverChampion: ""))
+           
+ContentView(SeasonToPass: Season(id: 1, image: "", name: "MGP W01", season: "2010", drivers: ["Nico", "Michael"], championshipPoints: "214", teamChampionship: false, driversChampionship: false, driverChampion: ""))
                 
         }
     }
